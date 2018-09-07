d8de820d60e28de9ed7da98d78ccba0452d9ac3f
hacpaka <hacpaka@eggbe.com>
2018-09-07 15:13:34 +0300

    +

--- src/TStatic.php
+++ src/TStatic.php
@@ -1,13 +1,14 @@
 <?php
 namespace Able\Statics;
 
+use Exception;
+
 trait TStatic {
 
 	/**
-	 * @throws \Exception
+	 * @throws Exception
 	 */
 	public final function __construct() {
-		throw new \Exception(sprintf('Can not create an instance of the static class: %s!', static::class));
+		throw new Exception(sprintf('Can not create an instance of the static class: %s!', static::class));
 	}
-
 }
